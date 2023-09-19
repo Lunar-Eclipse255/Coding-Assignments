2ae07bd17da59cb285f8bd1897465c56c03fde9b
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-09-18 21:46:01 -0400

    update lab 12 to include nested loops

--- AP CSA/Lab 12: Multiplication Table/Lab_12.java
+++ AP CSA/Lab 12: Multiplication Table/Lab_12.java
@@ -9,9 +9,15 @@ public class Lab_12 {
         //Asks for a number from the user and stores it
         System.out.print("Give a number: ");
         int userNum = scanner.nextInt();
+        //Prints what table the number is for
+        System.out.println("Table for "+userNum+":" );
         //Prints the multiplication table starting at 1 up to and including n, with tabs between numbers
         for (int i =1; i<=userNum;i++){
-            System.out.println(i*1 + "\t" + i*2 +"\t"+i*3+"\t"+i*4+"\t"+i*5+"\t"+i*6+"\t"+i*7+"\t"+i*8+"\t"+i*9+"\t"+i*10);
+            for(int j=1; j<=10; j++){
+                System.out.print(i*j+"\t");
+            }
+            //Goes down a line
+            System.out.println();
         }
         scanner.close();
     }
