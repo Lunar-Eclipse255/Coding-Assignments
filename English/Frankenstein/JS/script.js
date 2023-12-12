6e958423c6bbaf1443b483cb36edf4d6216910bc
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-12-12 01:59:29 -0500

    checkpoint

--- English/Frankenstein/JS/script.js
+++ English/Frankenstein/JS/script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const articles = [
-        { title: 'Trial of Justine Moritz', content: 'Content for Article 1'},
+        { title: 'Trial of Justine Moritz', content: 'Content for Article 1', imageSrc: "" },
         { title: 'Murder of Henry Clerval', content: 'Content for Article 2' },
         { title: 'From Marriage to Death: Murder of Elizabeth Frankenstein', content: 'Content for Article 3'},
-        { title: 'Scientist Gone Mad', content: 'Content for Article 4' },
+        { title: 'Genevan Scientist Gone', content: 'Content for Article 4' },
         { title: 'Letters to the Editor', content: 'Content for Article 5'},
         { title: 'Obituaries', content: 'Content for Article 6' },
         { title: 'Personals', content: 'Content for Article 7'},
@@ -18,7 +18,9 @@ document.addEventListener('DOMContentLoaded', function () {
         const articleSquare = document.createElement('div');
         articleSquare.classList.add('article-square');
         articleSquare.innerHTML = `<h3>${article.title}</h3>`;
-
+        const img = document.createElement('img');
+        img.src = article.imageSrc;
+        articleSquare.appendChild(img);
         articleSquare.addEventListener('click', function () {
             window.location.href = `article${index + 1}.html`;
         });
@@ -26,5 +28,5 @@ document.addEventListener('DOMContentLoaded', function () {
         articleContainer.appendChild(articleSquare);
     });
 });
-document.getElementById('dynamic-footer').innerHTML = '<p>&copy;(Powered by Electricity)</p>';
+document.getElementById('dynamic-footer').innerHTML = '<p>(Powered by Electricity)</p>';
 
